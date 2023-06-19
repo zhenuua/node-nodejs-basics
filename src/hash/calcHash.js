27f5a85f2d041a345cda0a1724b8cc27d86597f8
Yauheni Zakharenka <47642482+zhenuua@users.noreply.github.com>
2023-06-20 00:58:30 +0300

    Hash task: Implementing

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import fs from 'node:fs';
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    const { createHash } = await import('node:crypto');
+    const hash = createHash('sha256');
+    const data = fs.readFileSync('./src/hash/files/fileToCalculateHashFor.txt');
+    hash.update(data);
+    console.log(hash.digest('hex'));
 };
 
 await calculateHash();
