d7a56acbf0fbbe3fead64d72db8821efbab79f8f
Yauheni Zakharenka <47642482+zhenuua@users.noreply.github.com>
2023-06-20 01:48:04 +0300

    Zlib tasks: Implementing

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+
 const compress = async () => {
-    // Write your code here 
+    const input = 'src/zip/files/fileToCompress.txt';
+    const output = 'src/zip/files/archive.gz';
+    try {
+        const pipe = promisify(pipeline);
+        const gzip = createGzip();
+        const source = createReadStream(input);
+        const destination = createWriteStream(output);
+        await pipe(source, gzip, destination);
+    }
+    catch {
+        console.error('An error occurred:', err);
+        process.exitCode = 1;
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createUnzip } from 'zlib';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+
+
 const decompress = async () => {
-    // Write your code here 
+    const input = 'src/zip/files/archive.gz';
+    const output = 'src/zip/files/fileToCompress.txt';
+    try {
+        const pipe = promisify(pipeline);
+        const gzip = createUnzip();
+        const source = createReadStream(input);
+        const destination = createWriteStream(output, {
+            encoding: 'utf-8'
+        });
+        await pipe(source, gzip, destination);
+    }
+    catch {
+        console.error('An error occurred:', err);
+        process.exitCode = 1;
+    }
 };
 
 await decompress();
